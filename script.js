ffdaccda2c14f0bbb355f0135a5a2ae6d06b1411
Jinji <127928909+ProstoGio@users.noreply.github.com>
2026-01-02 14:40:03 +0400

    Correct line break tags in ninjatext
    Fix HTML line breaks in ninjatext.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button=document.getElementById("ninjabutton");
 let ind = false;
 button.addEventListener("click", function(){
   if(ind == false){
-    document.getElementById("ninjatext").innerHTML="</br></br>";
+    document.getElementById("ninjatext").innerHTML="<br><br>";
     ind = true;
   } else {
     document.getElementById("ninjatext").innerHTML="hey I'm text right? But I am also a Ninja.</br>Want proof ? click here -> ";
